d7804f1484dbecc20bcb93012b0017a53126700b
Opilane TTHK <opilane@tthk.ee>
2024-10-03 11:27:31 +0300

    pooleli 7.49 video

---
+++ ChatApp/ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(0 => _server.ConnectToServer());
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Net/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client;
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToServer()
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+            }
+        }
+    }
+}
